70f051398168ce61f3be5a2a415b0f2792a586e3
sergsmirno47 <131863515+sergsmirno47@users.noreply.github.com>
2024-03-02 21:12:10 +0200

    Update api.php add apiFilm route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('apiFilms', \App\Http\Controllers\Test1\ApiFilmController::class);
